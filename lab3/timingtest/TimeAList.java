72f3575bdb980e98820c9d54d9326668af3f96ac
liuyuhan <tyco18929289657@qq.com>
2025-09-22 23:57:09 +0800

    finish lab3

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -1,4 +1,5 @@
 package timingtest;
+
 import edu.princeton.cs.algs4.Stopwatch;
 
 /**
@@ -23,5 +24,22 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        AList<Integer> test = new AList<>();
+        for(int i = 1000;i<=64000;i*=2){
+            Ns.addLast(i);
+            opCounts.addLast(i);
+            int j = 0;
+            Stopwatch sw = new Stopwatch();
+            while(j<i){
+                test.addLast(1);
+                j+=1;
+            }
+            double timeInSeconds = sw.elapsedTime();
+            times.addLast(timeInSeconds);
+        }
+        printTimingTable(Ns,times,opCounts);
     }
 }
